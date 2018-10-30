331a8fba77d7f73d1e456b822572ebd917e4db48
Siriondil0 <elros.du.13@gmail.com>
2018-10-30 12:10:50 +0100

    dernier form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  protect_from_forgery with: :exception
+  def index 
+  end
+  def new
+    #A décommenter pour les méthode avec Form_tag et Form_for
+    @user=User.new
+  end
+  def create
+    #A décommenter pour la méthode avec Form
+    # User.create!(username: params["user_username"], email: params["user_email"], bio: params["user_bio"])
+    #A décommenter pour la méthode avec Form_tag
+    #User.create!(username: params[:username], email: params[:email], bio: params[:bio])
+    #A décommenter pour la méthode avec Form_for
+    User.create!(username: params[:user][:username], email: params[:user][:email], bio: params[:user][:bio])
+    redirect_to('/new')
+  end
+end
