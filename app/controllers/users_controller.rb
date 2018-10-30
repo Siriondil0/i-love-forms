980d3e2ecc34035dd1d169b97d8e12493ef8458c
Siriondil0 <elros.du.13@gmail.com>
2018-10-30 14:42:10 +0100

    routes separee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,16 @@ class UsersController < ApplicationController
   def create_form
     User.create!(username: params["user_username"], email: params["user_email"], bio: params["user_bio"])
   end
-  def new
-    #A décommenter pour les méthode avec Form_tag et Form_for
+  def new_form_tag
     @user=User.new
   end
-  def create
-    #A décommenter pour la méthode avec Form
-    # 
-    #A décommenter pour la méthode avec Form_tag
-    #User.create!(username: params[:username], email: params[:email], bio: params[:bio])
-    #A décommenter pour la méthode avec Form_for
+  def create_form_tag
+    User.create!(username: params[:username], email: params[:email], bio: params[:bio])
+  end
+  def new_form_for
+    @user=User.new
+  end
+  def create_form_for
     User.create!(username: params[:user][:username], email: params[:user][:email], bio: params[:user][:bio])
     redirect_to('/new')
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'user#index'
+  root 'users#index'
   get '/new_form', to: 'users#new_form'
   get '/new_form_tag', to: 'users#new_tag'
   get '/new_form_for', to: 'users#new_for'
